059cdfe8a5cc13f776df5a5a32fc1e0c8462a912
ucwiao <arif@fbi.one>
2025-07-29 15:32:41 +0530

    Update socials.js

--- src/commands/socials.js
+++ src/commands/socials.js
@@ -9,7 +9,7 @@ export const socials = {
         <a href="https://github.com/nigga" target="_blank" rel="noopener noreferrer" className="link-color">github.com</a>
       </li>
       <li>
-        <span className="command-color">linkedin</span><span className="mx-2">-</span>
+        <span className="command-color">Discord</span><span className="mx-2">-</span>
         <a href="https://discord.gg/MMfnpcW9" target="_blank" rel="noopener noreferrer" className="link-color">Discord</a>
       </li>
     </ul>
