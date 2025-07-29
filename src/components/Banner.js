4146044ad2882c01e3e9e9c57b40387f01854263
ucwiao <arif@fbi.one>
2025-07-29 15:39:12 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,13 +3,9 @@ import React from 'react';
 const Banner = () => (
   <div>
     <pre className="output-color text-xs md:text-base">
-{`   ██████╗ ██╗   ██╗███████╗███████╗
-  ██╔════╝ ██║   ██║██╔════╝██╔════╝
-  ██║  ███╗██║   ██║███████╗█████╗  
-  ██║   ██║██║   ██║╚════██║██╔══╝  
-  ╚██████╔╝╚██████╔╝███████║███████╗
-   ╚═════╝  ╚═════╝ ╚══════╝╚══════╝`}
-    </pre>
+{`   
+█▄░█ █ █▀▀ ▄▀█ █▀█ █▀█ █ █▀▄   ▀█ █░█
+█░▀█ █ █▄█ █▀█ █▀▄ █▄█ █ █▄▀   █▄ ▀▄▀  </pre>
     <div className="output-color mt-4">Welcome to my interactive terminal.</div>
     <div className="output-color">Type <span className="command-color">'help'</span> to see a list of available commands.</div>
   </div>
