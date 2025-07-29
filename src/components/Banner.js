f16785988e20687520e28f5873e7abf76b457ea0
ucwiao <arif@fbi.one>
2025-07-29 15:45:12 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 const Banner = () => (
   <div>
+    {/* The template literal starts with the backtick ` on the next line */}
     <pre className="output-color text-xs md:text-base">
-{`   
-█▄░█ █ █▀▀ ▄▀█ █▀█ █▀█ █ █▀▄   ▀█ █░█
-█░▀█ █ █▄█ █▀█ █▀▄ █▄█ █ █▄▀   █▄ ▀▄▀  </pre>
+{`███╗░░░███╗██╗░██████╗░░█████╗░██████╗░░█████╗░
+████╗░████║██║██╔════╝░██╔══██╗██╔══██╗██╔══██╗
+██╔████╔██║██║██║░░██╗░███████║██████╔╝██║░░██║
+██║╚██╔╝██║██║██║░░╚██╗██╔══██║██╔══██╗██║░░██║
+██║░╚═╝░██║██║╚██████╔╝██║░░██║██║░░██║╚█████╔╝
+╚═╝░░░░░╚═╝╚═╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░
+
+██████╗░██╗░░░██╗
+╚════██╗██║░░░██║
+░░███╔═╝╚██╗░██╔╝
+██╔══╝░░░╚████╔╝░
+███████╗░░╚██╔╝░░
+╚══════╝░░░╚═╝░░░`}
+{/* The template literal is closed with the backtick ` right above this comment */}
+    </pre>
     <div className="output-color mt-4">Welcome to my interactive terminal.</div>
     <div className="output-color">Type <span className="command-color">'help'</span> to see a list of available commands.</div>
   </div>
