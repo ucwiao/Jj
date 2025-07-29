6b939dd76ed471ae90208e3869cf9856e6cbab5d
ucwiao <arif@fbi.one>
2025-07-29 15:02:29 +0530

    Create Banner.js

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Banner = () => (
+  <div>
+    <pre className="output-color text-xs md:text-base">
+{`   ██████╗ ██╗   ██╗███████╗███████╗
+  ██╔════╝ ██║   ██║██╔════╝██╔════╝
+  ██║  ███╗██║   ██║███████╗█████╗  
+  ██║   ██║██║   ██║╚════██║██╔══╝  
+  ╚██████╔╝╚██████╔╝███████║███████╗
+   ╚═════╝  ╚═════╝ ╚══════╝╚══════╝`}
+    </pre>
+    <div className="output-color mt-4">Welcome to my interactive terminal.</div>
+    <div className="output-color">Type <span className="command-color">'help'</span> to see a list of available commands.</div>
+  </div>
+);
+
+export default Banner;
