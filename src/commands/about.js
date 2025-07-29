e88bd490fa041f55a2fb052f2a6c554195c87606
ucwiao <arif@fbi.one>
2025-07-29 15:12:26 +0530

    Create about.js

---
+++ src/commands/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const about = {
+  description: 'A little bit about me.',
+  execute: () => (
+    <p className="output-color">
+      Yall already know me very well
+    </p>
+  )
+};
