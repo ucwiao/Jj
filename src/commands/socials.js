6ae83fdbbc2bdde2f771e10a0972a08d3f3fe249
ucwiao <arif@fbi.one>
2025-07-29 15:17:19 +0530

    Create socials.js

---
+++ src/commands/socials.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const socials = {
+  description: 'Lists my social media profiles.',
+  execute: () => (
+    <ul className="output-color">
+      <li>
+        <span className="command-color">github</span><span className="mx-2">-</span>
+        <a href="https://github.com/nigga" target="_blank" rel="noopener noreferrer" className="link-color">github.com</a>
+      </li>
+      <li>
+        <span className="command-color">linkedin</span><span className="mx-2">-</span>
+        <a href="https://discord.gg/MMfnpcW9" target="_blank" rel="noopener noreferrer" className="link-color">Discord</a>
+      </li>
+    </ul>
+  )
+};
+    
